7d3dae95e913bdbcf05932cd871762b289e992c3
Roman Luitko <dossalab@pm.me>
2022-12-31 11:10:47 +0100

    init commit

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <nrf_gpio.h>
+
+#include "ble.h"
+
+#define LED_PIN		27
+
+static inline void led_switch(bool on) {
+	nrf_gpio_pin_write(LED_PIN, on);
+}
+
+static inline void led_init(void) {
+	nrf_gpio_pin_dir_set(LED_PIN, NRF_GPIO_PIN_DIR_OUTPUT);
+	led_switch(false);
+}
+
+static void connection_events_handler(ble_evt_t const *event, void *user)
+{
+	switch (event->header.evt_id) {
+	case BLE_GAP_EVT_CONNECTED:
+		led_switch(true);
+		break;
+
+	case BLE_GAP_EVT_DISCONNECTED:
+		led_switch(false);
+		break;
+	}
+}
+
+NRF_SDH_BLE_OBSERVER(connection_observer, BLE_C_OBSERVERS_PRIORITY, connection_events_handler, NULL);
+
+void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t _info)
+{
+	__disable_irq();
+	led_switch(true);
+
+	while(1);
+}
+
+int main(void)
+{
+	led_init();
+	ble_c_init();
+
+	for (;;) {
+		sd_app_evt_wait();
+	}
+}
